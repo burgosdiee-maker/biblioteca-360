8e9d0004202f2b57c00d6f5707ca25b6612ff154
burgosdiee-maker <burgosdiee@gmail.com>
2026-06-12 14:36:16 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,215 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bosques_palermo1",
+      "name": "Bosques_palermo1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 3.022114270244657,
+        "pitch": 0.1373900944242088,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1279723860075848,
+          "pitch": 0.16726929359600007,
+          "rotation": 0,
+          "target": "2-bosques_palermo3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.1279723860075848,
+          "pitch": 0.16726929359600007,
+          "title": "Rosedal",
+          "text": "Rosas silvestres: floren de octubre a diciembre. Epoca de poda; agosto."
+        }
+      ]
+    },
+    {
+      "id": "1-bosques_palermo2",
+      "name": "Bosques_palermo2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bosques_palermo3",
+      "name": "Bosques_palermo3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -3.101455417241681,
+        "pitch": 0.08641051823699364,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1070929029333296,
+          "pitch": 0.04930524673695125,
+          "rotation": 0,
+          "target": "3-bosques_palermo4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.080714615841707,
+          "pitch": 0.0656697168370215,
+          "title": "Parque de los poetas",
+          "text": "En el recorrido podes encontrar a los poetas mas importantes de la antiguedad."
+        }
+      ]
+    },
+    {
+      "id": "3-bosques_palermo4",
+      "name": "Bosques_palermo4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": -0.05078178298683156,
+        "pitch": -0.02544261007854587,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.02199262094780785,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-bosques_palermo5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.05078178298683156,
+          "pitch": -0.02544261007854587,
+          "title": "Puente del Rosedal",
+          "text": "EL puente es conocido por por su popularidad entre los novios."
+        }
+      ]
+    },
+    {
+      "id": "4-bosques_palermo5",
+      "name": "Bosques_palermo5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "yaw": 2.0031262978300948,
+        "pitch": 0.0018503716420958938,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.0031262978300948,
+          "pitch": 0.0018503716420958938,
+          "title": "Bosques de Palermo",
+          "text": "Los bosques poseen 1000 hectreas que fueron donadoas por Las Heras."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
